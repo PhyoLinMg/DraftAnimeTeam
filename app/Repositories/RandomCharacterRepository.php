db0c55a917fa7e1beec846ca3ebb2cd052da9ad1
PhyoLinMg <gangsterplm@gmail.com>
2025-01-24 11:30:35 +0800

    update logic on board type in randoming character

--- app/Repositories/RandomCharacterRepository.php
+++ app/Repositories/RandomCharacterRepository.php
@@ -3,15 +3,26 @@ namespace App\Repositories;
 
 use App\Models\Character;
 use App\Models\Battle;
+use App\Models\Anime;
+use App\Enums\BoardType;
 
 class RandomCharacterRepository{
     public function pickRandomCharacter(int $battleId): Character{
         $battle= Battle::find($battleId);
+        $board= $battle->board;
         $characters= $battle->characters()->get();
         $pivotValues = array_column($characters->toArray(), 'pivot'); 
         $characterIdsPickedByPlayers = array_column($pivotValues, 'character_id'); 
+        
+        if($board->type== BoardType::All){
+            $randomCharacter= Character::whereNotIn('id',$characterIdsPickedByPlayers)->inRandomOrder()->first();
+        }
+        else{
+            $animes= Anime::whereIn('name',['One Piece','Bleach','Naruto']);
+            $randomCharacter= $animes->characters()->whereNotIn('id',$characterIdsPickedByPlayers)->inRandomOrder()->first();
+        }
 
-        $randomCharacter= Character::whereNotIn('id',$characterIdsPickedByPlayers)->inRandomOrder()->first();
+        // for all character 
     
         return $randomCharacter;
     }
